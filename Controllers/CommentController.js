e87969cbee7fbb580bf748ccaa089a1ac218f9eb
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-25 00:46:31 +0300

    basic controllr func...

---
+++ Controllers/CommentController.js
@@ -0,0 +1,36 @@
+const Comment =require("../models/comment")
+
+
+exports.getComments = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+
+exports.postComments = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+
+exports.putComments = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+exports.deleteComments = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
