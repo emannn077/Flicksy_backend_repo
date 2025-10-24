8591b74475c2819596369b278eb14fafa1185db4
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-25 00:30:04 +0300

    routers

---
+++ routes/commentRoute.js
@@ -0,0 +1,12 @@
+const router =  require("express").Router()
+const commentctrl = require("../Controllers/CommentController")
+
+
+
+router.get("",)
+router.post("")
+router.put("")
+router.delete("")
+
+
+module.exports = router
