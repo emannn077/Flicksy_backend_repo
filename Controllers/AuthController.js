31acddc5f567a153391cc3a0e4a1f212d6be3a9b
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-28 19:07:08 +0300

    pulling

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user")
+const User = require("../models/User")
 const middleware = require("../middleware")
 
 const auth_signup_post = async (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 require("dotenv").config()
 const path = require("path")
-
+const cors = require("cors")
 // Database
 const mongoose = require("./db/index")
 
@@ -10,28 +10,29 @@ const methodOverride = require("method-override")
 const morgan = require("morgan")
 
 // Routers
-const authRouter = require('./routes/auth')
-const userRouter = require('./routes/user')
-const commentRouter = require('./routes/commentRoute')
-const challengeRouter = require('./routes/challengeRoute')
-const postRouter = require('./routes/postRoute')
+const authRouter = require("./routes/auth")
+const userRouter = require("./routes/user")
+const commentRouter = require("./routes/commentRoute")
+const challengeRouter = require("./routes/challengeRoute")
+const postRouter = require("./routes/postRoute")
 
 const app = express()
 const port = process.env.PORT || 3001
 
 // ===== MIDDLEWARES =====
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 app.use(express.json())
 app.use(methodOverride("_method"))
 app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname, "public")))
 
 // ===== ROUTES =====
-app.use('/auth', authRouter) // Auth APIs
-app.use('/users', userRouter) // Protected user APIs
-app.use('/comment', commentRouter)
-app.use('/challenge', challengeRouter)
-app.use('/post', postRouter)
+app.use("/auth", authRouter) // Auth APIs
+app.use("/users", userRouter) // Protected user APIs
+app.use("/comment", commentRouter)
+app.use("/challenge", challengeRouter)
+app.use("/post", postRouter)
 
 // ===== START SERVER =====
 app.listen(port, () => {
